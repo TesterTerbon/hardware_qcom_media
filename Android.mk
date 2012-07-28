CyanogenMod/android_hardware_qcom_media

Add support for 7630_surf for JB bringup.
Change-Id: I7599449e56c26cbbc46f084567bf71da95ccf099

Android.mk
@@ -1,9 +1,9 @@
 QCOM_MEDIA_ROOT := $(call my-dir)
-ifneq ($(filter msm8960 msm8660 msm7627a,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8960 msm8660 msm7627a msm7630_surf,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
 include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
 endif
 
-ifneq ($(filter msm8960 msm8660,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8960 msm8660 msm7630_surf,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-video/Android.mk
 endif

mm-video/vidc/vdec/Android.mk
@@ -42,6 +42,9 @@ endif
 ifeq ($(TARGET_BOARD_PLATFORM),msm7627a)
 libOmxVdec-def += -DMAX_RES_720P
 endif
+ifeq ($(TARGET_BOARD_PLATFORM),msm7630_surf)
+libOmxVdec-def += -DMAX_RES_720P
+endif
 
 libOmxVdec-def += -D_ANDROID_ICS_
 

mm-video/vidc/venc/Android.mk
@@ -36,6 +36,9 @@ endif
 ifeq ($(TARGET_BOARD_PLATFORM),msm7627a)
 libmm-venc-def += -DMAX_RES_720P
 endif
+ifeq ($(TARGET_BOARD_PLATFORM),msm7630_surf)
+libmm-venc-def += -DMAX_RES_720P
+endif
 ifeq ($(TARGET_USES_ION),true)
 libmm-venc-def += -DUSE_ION
 endif

TesterTerbon/hardware_qcom_media

Android.mk
@@ -1,10 +1,10 @@
 QCOM_MEDIA_ROOT := $(call my-dir)
-ifneq ($(filter msm8960 msm8660 msm7627a,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8960 msm8660 msm7627a msm7x30,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
 include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
 endif
 
-ifneq ($(filter msm8960 msm8660,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8960 msm8660 msm7x30,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-video/Android.mk
 include $(QCOM_MEDIA_ROOT)/libI420colorconvert/Android.mk
 endif

mm-video/vidc/vdec/Android.mk
@@ -42,6 +42,9 @@ endif
 ifeq ($(TARGET_BOARD_PLATFORM),msm7627a)
 libOmxVdec-def += -DMAX_RES_720P
 endif
+ifeq ($(TARGET_BOARD_PLATFORM),msm7x30)
+libOmxVdec-def += -DMAX_RES_720P
+endif
 
 libOmxVdec-def += -D_ANDROID_ICS_
 

mm-video/vidc/venc/Android.mk
@@ -36,6 +36,9 @@ endif
 ifeq ($(TARGET_BOARD_PLATFORM),msm7627a)
 libmm-venc-def += -DMAX_RES_720P
 endif
+ifeq ($(TARGET_BOARD_PLATFORM),msm7x30)
+libmm-venc-def += -DMAX_RES_720P
+endif
 ifeq ($(TARGET_USES_ION),true)
 libmm-venc-def += -DUSE_ION
 endif