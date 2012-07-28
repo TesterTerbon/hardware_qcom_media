CyanogenMod/android_hardware_qcom_media

mm-video: Enable compilation on MSM8974 target
Enable compilation of and fix compilation issues of
mm-video module for MSM8974.  Change references of
MSM target 'copper' to 'msm8974'.

Change-Id: If3d975e840f08ab74396590cec6ab18f3e55c6a5

Android.mk
@@ -1,10 +1,10 @@
 QCOM_MEDIA_ROOT := $(call my-dir)
-ifneq ($(filter msm8960 msm8660 msm7627a msm7630_surf,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8974 msm8960 msm8660 msm7627a msm7630_surf,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
 include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
 endif
 
-ifneq ($(filter msm8960 msm8660 msm7630_surf,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8974 msm8960 msm8660 msm7630_surf,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-video/Android.mk
 include $(QCOM_MEDIA_ROOT)/libI420colorconvert/Android.mk
 endif

TesterTerbon/hardware_qcom_media

Conflicts:
	Android.mk

Android.mk
@@ -1,10 +1,10 @@
 QCOM_MEDIA_ROOT := $(call my-dir)
-ifneq ($(filter msm8960 msm8660 msm7627a msm7x30,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8974 msm8960 msm8660 msm7627a msm7x30,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
 include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
 endif
 
-ifneq ($(filter msm8960 msm8660 msm7x30,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8974 msm8960 msm8660 msm7x30,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-video/Android.mk
 include $(QCOM_MEDIA_ROOT)/libI420colorconvert/Android.mk
 endif